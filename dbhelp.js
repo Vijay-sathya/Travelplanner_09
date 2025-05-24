d091b29e5ea63608914bbc922ad5a376e816b6ae
Vijay-sathya <vijay.s1@s.amity.edu>
2025-05-24 15:41:44 +0530

    connected data base

---
+++ dbhelp.js
@@ -0,0 +1,75 @@
+~
+    connect() {
+        return new Promise((resolve, reject) => {
+            this.db = new sqlite3.Database(this.dbPath, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    console.log('Connected to SQLite database');
+                    resolve();
+                }
+            });
+        });
+    }
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+dbhelp.js [unix] (05:29 01/01/1970)                                                             0,1 All
+-- INSERT --
+
